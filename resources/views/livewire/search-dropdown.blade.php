eac5a64936cff5a40226ed1cc542c11095e533d4
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-02-09 23:01:04 -0400

    Add alpineJS in search dropdown and modal

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,21 @@
-<div class="relative mt-3 md:mt-0">
-  <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" x-on:click.outside="isOpen = false">
+  <input
+    wire:model.debounce.500ms="search"
+    type="text"
+    class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+    placeholder="Search"
+    x-ref="search"
+    x-on:keydown.window="
+      if (event.keyCode == 191) {
+        event.preventDefault();
+        $refs.search.focus();
+      }
+    "
+    x-on:focus="isOpen = true"
+    x-on:keydown="isOpen = true"
+    x-on:keydown.escape.window="isOpen = false"
+    x-on:keydown.shift.tab="isOpen = false"
+  >
   <div class="absolute top-0">
     <i class="fill-current text-gray-500 mt-1.5 ml-2 fas fa-search"></i>
   </div>
@@ -7,12 +23,20 @@
   <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
 
   @if (strlen($search) >= 2)
-    <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+    <div
+      class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4"
+      x-show="isOpen"
+      x-transition
+    >
       @if ($searchResults->count() > 0)
         <ul>
           @foreach ($searchResults as $result)
             <li class="border-b border-gray-700">
-              <a href="{{ route('movies.show', $result['id']) }}" class="hover:bg-gray-700 px-3 py-3 flex items-center">
+              <a
+                href="{{ route('movies.show', $result['id']) }}"
+                class="hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
+                @if ($loop->last) @keydown.tab="isOpen = false" @endif
+              >
                 @if ($result['poster_path'])
                   <img src="https://image.tmdb.org/t/p/w92{{ $result['poster_path'] }}" alt="poster" class="w-8">
                 @else
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -41,14 +41,48 @@
           </div>
         </div>
 
-        @if (count($movie['videos']['results']) > 0)
-          <div class="mt-12">
-            <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}" class="inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-              <i class="w-6 fill-current far fa-play-circle"></i>
-              <span class="ml-2">Play trailer</span>
-            </a>
+        <div x-data="{ isOpen: false }">
+          @if (count($movie['videos']['results']) > 0)
+            <div class="mt-12">
+              <button
+                x-on:click="isOpen = true"
+                class="inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
+              >
+                <i class="w-6 fill-current far fa-play-circle"></i>
+                <span class="ml-2">Play trailer</span>
+              </button>
+            </div>
+          @endif
+
+          <div
+            style="background-color: rgba(0, 0, 0, .5);"
+            class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+            x-show="isOpen"
+            x-transition
+          >
+            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+              <div class="bg-gray-900 rounded">
+                <div class="flex justify-end pr-4 pt-2">
+                  <button x-on:click="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;</button>
+                </div>
+                <div class="modal-body px-8 py-8">
+                  <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                    <iframe
+                      width="560"
+                      height="315"
+                      class="responsive-iframe absolute top-0 left-0 w-full h-full"
+                      src="https://youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}"
+                      style="border: 0;"
+                      allow="autoplay; encrypted-media"
+                      frameborder="0"
+                      allowfullscreen></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        @endif
+        </div>
+
       </div>
     </div>
   </div><!-- end movie-info -->
@@ -76,20 +110,51 @@
     </div>
   </div><!-- end movie-cast -->
 
-  <div class="movie-images">
+  <div class="movie-images" x-data="{ isOpen: false, image: ''}">
     <div class="container mx-auto px-4 py-16">
       <h2 class="text-4xl font-semibold">Images</h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
         @foreach ($movie['images']['backdrops'] as $image)
           @if ($loop->index < 9)
             <div class="mt-8">
-              <a href="#">
-                <img src="{{ "https://image.tmdb.org/t/p/w500{$image['file_path']}" }}" alt="image" class="hover:opacity-75 transition ease-in-out duration-150">
+              <a
+                x-on:click.prevent="
+                  isOpen = true
+                  image = '{{ "https://image.tmdb.org/t/p/original{$image["file_path"]}" }}'
+                "
+                href="#"
+              >
+                <img src="{{ "https://image.tmdb.org/t/p/w500{$image["file_path"]}" }}" alt="image" class="hover:opacity-75 transition ease-in-out duration-150">
               </a>
             </div>
           @endif
         @endforeach
       </div>
+
+      <div
+        style="background-color: rgba(0, 0, 0, .5);"
+        class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+        x-show="isOpen"
+        x-transition
+      >
+        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+          <div class="bg-gray-900 rounded">
+            <div class="flex justify-end pr-4 pt-2">
+              <button
+                x-on:click="isOpen = false"
+                x-on:keydown.escape.window="isOpen = false"
+                class="text-3xl leading-none hover:text-gray-300"
+              >
+                &times;
+              </button>
+            </div>
+            <div class="modal-body px-8 py-8">
+              <img :src="image" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div><!-- end movie-images -->
 
